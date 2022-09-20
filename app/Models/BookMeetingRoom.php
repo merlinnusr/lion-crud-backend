67fcdf31d5230d7e3a71c26d491966ed5e2d0582
plomero <plomero@gmail.com>
2022-09-19 21:41:32 -0500

    Anadi relacion del modelo BookMeetingRoom

--- app/Models/BookMeetingRoom.php
+++ app/Models/BookMeetingRoom.php
@@ -8,9 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class BookMeetingRoom extends Model
 {
     use HasFactory;
+    // protected $with = ['meeting_rooms'];
     protected $fillable = [
         'meeting_date_start',
         'meeting_date_end',
         'meeting_room_id',
     ];
+    public function meeting_rooms()
+    {
+        return $this->belongsTo(MeetingRoom::class,'meeting_room_id', 'id');
+    }
 }
